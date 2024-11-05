9a407f280486d56ded52603d3da5a6a3b69ee7b1
Michael Samsom <mike.samsom@asica.ca>
2024-11-05 13:04:04 -0700

    removed placeholder func

--- src/infotheory.rs
+++ src/infotheory.rs
@@ -1,9 +1,5 @@
 pub use crate::error::Result;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 pub fn log_ns<T>(v: T) -> T
 where
     T: num_traits::Float,
@@ -20,12 +16,6 @@ mod tests {
     use ndarray::{arr1, Array};
 
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     #[test]
     fn test_log_ns() {
